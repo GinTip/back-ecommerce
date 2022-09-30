8f20ed55ab2daca0f2411aea912cd2fe4113ba9e
Gina Tipa Herrera <gitipa.uxuic2@ucamp.utel.edu.mx>
2022-09-30 17:43:38 -0500

    configuraciones

--- src/middlewares/jwt.middleware.js
+++ src/middlewares/jwt.middleware.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken")
 const User = require("../models/user")
 
 const validarJWT = async (req, res, next) => {
-  const token = req.header("x-auth-token");
+  const token = req.header("auth-token");
 
   if (!token) {
     return res.status(401).json({
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -10,7 +10,7 @@ const {
 
 const router = Router();
 
-router.get("/", getProduct);
+router.get("/", validarJWT, getProduct);
 router.post("/", postProduct);
 router.put("/:id", updateProduct);
 router.delete("/:id", deleteProduct);
